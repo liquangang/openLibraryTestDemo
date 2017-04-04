5c22fe289bec5aed221d033b7e7cbbfa4392550a
liquangang <sgliquangang@163.com>
2017-04-04 15:38:57 +0800

    Initial Commit

---
+++ openLibraryTestDemo/openLibraryTestDemo.h
@@ -0,0 +1,19 @@
+//
+//  openLibraryTestDemo.h
+//  openLibraryTestDemo
+//
+//  Created by liquangang on 2017/4/4.
+//  Copyright © 2017年 liquangang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openLibraryTestDemo.
+FOUNDATION_EXPORT double openLibraryTestDemoVersionNumber;
+
+//! Project version string for openLibraryTestDemo.
+FOUNDATION_EXPORT const unsigned char openLibraryTestDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openLibraryTestDemo/PublicHeader.h>
+
+
